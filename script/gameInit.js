39e112320666f18672815c180baf22617914b12e
gbulgaru <bulgarugeorgeionut140@gmail.com>
2024-06-07 18:09:07 +0200

    Ver1.1 UI changes. Fixed the bug that displayed the name of the correct country instead of the correct flag in the "Name to Flag" game mode. New directory for library files.

--- script/gameInit.js
+++ script/gameInit.js
@@ -1,51 +1,97 @@
-let currentCountry = null; // Variabile globale per memorizzare il paese corrente
-let gameInProgress = false; // Flag per impedire richieste multiple
+let currentCountry = null;
+let gameInProgress = false; // Flag to prevent multiple requests
 
 $(document).ready(function() {
-	$('#startGame').on('click', function() {
-		startGame();
+	$('#flagToName').on('click', function() {
+		flagToName();
 		$(this).hide();
+		$('#nameToFlag').hide();
 	});
-	
+
+	$('#nameToFlag').on('click', function() {
+		nameToFlag();
+		$(this).hide();
+		$('#flagToName').hide();
+	});
+
 	$(document).on('click', '#verifyButton', function() {
 		if (gameInProgress) {
 			verifyAnswer();
-			setTimeout(startGame, 3000);
 		}
 	});
-	
-	$(document).on('keypress', '#flag-display input', function(e) {
-		if (e.which === 13 && gameInProgress) { // Codice 13 è il tasto Invio
+
+	$(document).on('keypress', '#responseArea input', function(e) {
+		if (e.which === 13 && gameInProgress) { // Code 13 is the Enter key
 			verifyAnswer();
-			setTimeout(startGame, 3000);
 		}
 	});
 });
 
-function startGame() {
-	gameInProgress = true; // Imposta il flag per indicare che il gioco è in corso
-	$("#flag-display, #result").empty();
-	
+function nameToFlag() {
+	gameInProgress = true; // Set the flag to indicate that the game is in progress
+	$("#initialPrompt").empty();
+	$("#responseArea").empty();
+
 	let selectedContinent = $("#continentSelect").val();
-	
-	$.getJSON(`https://restcountries.com/v3.1/region/${selectedContinent}`, function(countries) {
-		// Seleziona un paese a caso
+
+	$.getJSON(`https://restcountries.com/v3.1/region/${selectedContinent}?fields=name,cca2`, function(countries) {
+		// Select a random country
 		let randomCountry = countries[Math.floor(Math.random() * countries.length)];
-		currentCountry = randomCountry.name.common; // Memorizza il nome del paese
+		currentCountry = randomCountry; // Store the country object
 
-		// Visualizza l'emoji della bandiera
+		// Display the country name
+		$("#initialPrompt").text(randomCountry.name.common);
+
+		// Create and display the flags
+		let flagContainer = [];
+		for (let i = 0; i < 3; i++) {
+			let country = i === 0 ? randomCountry : countries[Math.floor(Math.random() * countries.length)];
+			let flagImage = $("<p>").text(countryCodeToFlagEmoji(country.cca2)).data("country", country.name.common);
+			flagContainer.push(flagImage);
+		}
+
+		// Shuffle the flags
+		flagContainer.sort(function() { return 0.5 - Math.random(); });
+
+		// Add the flags to the display and set the click event
+		flagContainer.forEach(flag => {
+			$("#responseArea").append(flag);
+			flag.on('click', function() {
+				if (gameInProgress) {
+					verifyFlagSelection($(this).data("country"));
+				}
+			});
+		});
+
+		$("#responseArea p").css("display", "inline");
+	});
+}
+
+function flagToName() {
+	gameInProgress = true; // Set the flag to indicate that the game is in progress
+	$("#initialPrompt").empty();
+	$("#responseArea").empty();
+
+	let selectedContinent = $("#continentSelect").val();
+
+	$.getJSON(`https://restcountries.com/v3.1/region/${selectedContinent}?fields=name,cca2`, function(countries) {
+		// Select a random country
+		let randomCountry = countries[Math.floor(Math.random() * countries.length)];
+		currentCountry = randomCountry; // Store the country object
+
+		// Display the flag emoji
 		let flagEmoji = countryCodeToFlagEmoji(randomCountry.cca2);
-		$("#flag-display").append($("<p>").text(flagEmoji));
+		$("#initialPrompt").text(flagEmoji);
 
-		// Crea una textbox per l'input dell'utente
-		let input = $("<input>").attr("type", "text").attr("placeholder", "Inserisci il nome del paese");
-		$("#flag-display").append(input);
+		// Create a textbox for user input
+		let input = $("<input>").attr("type", "text").attr("placeholder", "Write the country name in English");
+		$("#responseArea").append(input);
 
-		// Crea un pulsante di verifica
-		let verifyButton = $("<button>").attr("id", "verifyButton").text("Verifica");
-		$("#flag-display").append(verifyButton);
+		// Create a verify button
+		let verifyButton = $("<button>").attr("id", "verifyButton").text("Verify");
+		$("#responseArea").append(verifyButton);
 
-		input.focus(); // Metti a fuoco la textbox per l'input dell'utente
+		input.focus();
 	});
 }
 
@@ -53,16 +99,29 @@ function countryCodeToFlagEmoji(countryCode) {
 	return String.fromCodePoint(...[...countryCode.toUpperCase()].map(char => 127397 + char.charCodeAt()));
 }
 
-function verifyAnswer() {
-	if (!gameInProgress) return; // Evita richieste multiple
-
-	gameInProgress = false; // Impedisce ulteriori verifiche fino all'inizio del nuovo gioco
-	let userAnswer = $("#flag-display input").val().trim().toLowerCase();
-	if (userAnswer === currentCountry.toLowerCase()) {
-		$("#flag-display p").empty().text("✅");
+function verifyFlagSelection(selectedCountry) {
+	gameInProgress = false; // Prevent further verification until the new game starts
+	$("#responseArea").empty();
+	if (selectedCountry.toLowerCase() === currentCountry.name.common.toLowerCase()) {
+		$("#initialPrompt").text("✅");
 	} else {
-		let wrongAnswer = $("<div>").text("❌").addClass("wrong-answer");
-		let correctCountry = $("<div>").text(`La risposta corretta era ${currentCountry}`).addClass("correct-country");
-		$("#flag-display p").empty().append(wrongAnswer, correctCountry);
+		$("#initialPrompt").text("❌");
+		$("#responseArea").text("The correct answer was " + countryCodeToFlagEmoji(currentCountry.cca2));
 	}
+	setTimeout(nameToFlag, 2000);
+}
+
+function verifyAnswer() {
+	if (!gameInProgress) return; // Prevent multiple requests
+
+	gameInProgress = false; // Prevent further verification until the new game starts
+	let userAnswer = $("#responseArea input").val().trim().toLowerCase();
+	$("#responseArea").empty();
+	if (userAnswer === currentCountry.name.common.toLowerCase()) {
+		$("#responseArea").text("✅");
+	} else {
+		$("#initialPrompt").text("❌");
+		$("#responseArea").text(`The correct answer was ${currentCountry.name.common}`);
+	}
+	setTimeout(flagToName, 2000);
 }
